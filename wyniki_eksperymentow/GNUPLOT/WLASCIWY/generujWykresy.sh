e4006ead2e393b4788f3117d2bf3038805a86ddf
Revan2005 <tomek.biegus@gmail.com>
2016-04-23 22:46:25 +0200

    dodalem do skryptu gnuplota nowy wykres laczacy wszsytkie dane, taki jak ten moj javowy tylko lepszy bo z odchyleniami standardowymi, zapisuje go w tym samym katalogu co reszte wykresow i plik z parametrami

--- wyniki_eksperymentow/GNUPLOT/WLASCIWY/generujWykresy.sh
+++ wyniki_eksperymentow/GNUPLOT/WLASCIWY/generujWykresy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cd /home/tomek/workspace/MGR/ModelowanieEpidemii/wyniki_eksperymentow/GNUPLOT/WLASCIWY
-gnuplot s.script i.script r.script z.script
+gnuplot s.script i.script r.script z.script sirz.script
